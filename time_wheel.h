e713c049ff717193c10d4340c9d0b8475b91c5f9
Kijin Kim <kijin115@solvitsystem.co.kr>
2022-09-02 16:58:12 +0900

    Merge file

--- time_wheel.h
+++ time_wheel.h
@@ -2,7 +2,31 @@
 #define TIME_WHEEL_H
 
 #include <time.h>
-#include "circular_ll.h"
+
+/*=== Circular Linked List Implementation ===*/
+
+typedef struct _CircularLL
+{
+    struct _CircularLL *prev;
+    struct _CircularLL *next;
+} CircularLL;
+
+#define cll_foreach(trav_elem, head) \
+    for ((trav_elem) = (head)->next; \
+         (trav_elem) != (head);      \
+         (trav_elem) = (trav_elem)->next)
+
+#define cll_no_entry(head) \
+    ((head)->next == head)
+
+#define cll_one_entry(head) \
+    ((head)->next->next == head)
+
+int cll_add_tail(CircularLL *head, CircularLL *elem);
+int cll_delete(CircularLL *head, CircularLL *delete_elem);
+int cll_init_head(CircularLL *head);
+
+/*===========================================*/
 
 #define MAX_TWJOB_NAMELEN 100
 
